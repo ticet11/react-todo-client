8ba3c2e426de164c1cc375c7e0434fd3fe8f7893
Brian Kozub <ticet11@gmail.com>
2020-06-10 10:00:51 -0600

    handleSubmit and styling.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import './styles/main.css'
+
 class App extends React.Component {
     constructor() {
         super();
@@ -10,23 +12,29 @@ class App extends React.Component {
         };
     }
 
-    handleChange = (event) => {
-      this.setState({
-        todo: event.target.value
-      })
+    addTodoItem = (event) => {
+      event.preventDefault()
+      console.log(this.state, 'Form Submitted')
     }
+
+    handleChange = (event) => {
+        this.setState({
+            todo: event.target.value,
+        });
+    };
 
     render() {
         return (
             <div className="app">
                 <h1>To-Do Wizard</h1>
-                <form action="">
+                <form className='add-todo' onSubmit={this.addTodoItem}>
                     <input
                         type="text"
                         placeholder="Add Item"
                         onChange={this.handleChange}
                         value={this.state.todo}
                     />
+                <button type="submit">Cast</button>
                 </form>
             </div>
         );
