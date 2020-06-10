fb493c8b0421b5375242f067bc338d5e0555c327
Brian Kozub <ticet11@gmail.com>
2020-06-10 12:00:52 -0600

    Can mark todos as complete and incomplete in state and local db.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,44 @@
 import React from "react";
 
 export default class TodoItem extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: props.todoData.done
+        }
+    }
+
+    onCheck = () => {
+        fetch(`http://localhost:5000/todo/${this.props.todoData.id}`,{
+            method: 'PATCH',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+                done: !this.state.done
+            })
+        })
+        .then(() => {
+            this.setState({
+                done: !this.state.done
+            })
+        })
+        .catch(error => {
+            console.error('toggle done error', error)
+        })
+    }
 
     render() {
-        const { title, done } = this.props.todoData;
+        const { title } = this.props.todoData;
         return (
             <div className="todo-item">
-                <input type="checkbox" />
-                <p>{title}</p>
+                <input 
+                type="checkbox"
+                defaultChecked={this.state.done}
+                onClick={this.onCheck}
+                />
+                <p className={this.state.done ? 'done' : null} >
+                    {title}
+                </p>
             </div>
         );
     }
